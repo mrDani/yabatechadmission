fb15fbe0aa10050f7735bb861d8a7c9fa378ef91
Danny <Darnnielnology@gmail.com>
2021-08-27 00:53:35 +0100

    sqlite

--- yabatechadmissiondirect/settings.py
+++ yabatechadmissiondirect/settings.py
@@ -79,6 +79,13 @@ WSGI_APPLICATION = 'yabatechadmissiondirect.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'yabadb',
         'USER': 'postgres',
